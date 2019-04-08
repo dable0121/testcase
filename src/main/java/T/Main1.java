2bcb91ae6b2f49619bfc16ad437e9ce8d3d1cccf
daib <daib@glodon.com>
2019-04-08 14:48:09 +0800

    reflect

--- src/main/java/T/Main1.java
+++ src/main/java/T/Main1.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main1 {
 
   public static void main(String[] args) {
-    List<? super Apple> flist = new ArrayList<>();
+    List<? super Fruit> flist = new ArrayList<>();
     flist.add(new Apple());
     List<? extends Fruit> alist = new ArrayList<>();
 //    alist.add(new Fruit());
