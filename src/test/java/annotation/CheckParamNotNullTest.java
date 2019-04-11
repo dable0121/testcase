8c3c0b74af2b57163f9a438c950f6d18a14c1446
daib <daib@glodon.com>
2019-04-11 16:11:59 +0800

    customize annotation!

--- src/test/java/annotation/CheckParamNotNullTest.java
+++ src/test/java/annotation/CheckParamNotNullTest.java
@@ -2,6 +2,8 @@ package annotation;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,13 +13,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = Main.class)
 @ActiveProfiles("dev")
 public class CheckParamNotNullTest {
+  /** logger */
+  private static final Logger logger = LoggerFactory.getLogger(CheckParamNotNullTest.class);
 
   @Autowired
   private CheckMethodParamService checkMethodParam;
 
   @Test
   public void check() {
-    checkMethodParam.paramMethod(new ParamDTO(), "", 1);
+    try {
+      checkMethodParam.paramMethod(new ParamDTO(), "", 1);
+    } catch (Exception e) {
+     logger.error("检查错误成功");
+    }
     ParamDTO paramDTO = new ParamDTO();
     paramDTO.setParam("aaaaa");
     checkMethodParam.paramMethod(paramDTO, "", 1);
