c9ff81cc48614d57400929ecf4460d39c2b780f2
daib <daib@glodon.com>
2019-04-11 18:26:53 +0800

    bloom filter

--- src/test/java/bloomfilter/BloomTest.java
+++ src/test/java/bloomfilter/BloomTest.java
@@ -6,23 +6,26 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class BloomTest {
+
   @Test
   public void guavaTest() {
-    long star = System.currentTimeMillis();
     BloomFilter<Integer> filter = BloomFilter.create(
         Funnels.integerFunnel(),
-        10000000,
-        0.01);
+        100000000,
+        0.001);
 
-    for (int i = 0; i < 10000000; i++) {
+    //放数字
+    for (int i = 0; i < 100000000; i++) {
       filter.put(i);
     }
-
+    System.out.println("我放完了数值，开始判断---");
+    long star = System.currentTimeMillis();
     Assert.assertTrue(filter.mightContain(1));
     Assert.assertTrue(filter.mightContain(2));
     Assert.assertTrue(filter.mightContain(3));
-    Assert.assertFalse(filter.mightContain(10000000));
+    Assert.assertFalse(filter.mightContain(100000000));
+    Assert.assertTrue(filter.negate().test(1000000001));
     long end = System.currentTimeMillis();
-    System.out.println("执行时间：" + (end - star)+"毫秒");
+    System.out.println("执行时间：" + (end - star) + "毫秒");
   }
 }
