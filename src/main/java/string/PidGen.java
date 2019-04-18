5618055a10e697e3f0550d54c76617d271a98fa0
dable <daib@glodon.com>
2019-04-18 17:18:27 +0800

    id gen

--- src/main/java/string/PidGen.java
+++ src/main/java/string/PidGen.java
@@ -3,5 +3,6 @@ package string;
 public class PidGen {
     public static void main(String[] args) {
         System.out.println(String.format("P%05d", Long.parseLong("P00101".substring(1)) + 1));
+        System.out.println(String.format("P%05d", 1));
     }
 }
