a9cc59bdcec66ffbd1e28f183d9aa89d8b42cb8b
dable <daib@glodon.com>
2019-06-10 14:38:13 +0800

    map and filter use reduce

--- src/main/java/function/FunctionTest.java
+++ src/main/java/function/FunctionTest.java
@@ -15,6 +15,32 @@ public abstract class FunctionTest {
 
     private final static ThreadLocal<DateFormat> formatter =
             ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd"));
+    private static void testMultiReduce() {
+        ArrayList<List<String>> strings = new ArrayList<>();
+        strings.add(Arrays.asList("1", "2", "3", "4"));//10
+        strings.add(Arrays.asList("2", "3", "4", "5"));//14
+        strings.add(Arrays.asList("3", "4", "5", "6"));//18
+
+        // 非并行流
+        Integer reduce1 = strings.stream().flatMap(e -> e.stream()).reduce(0,
+                (acc, e) -> acc + Integer.valueOf(e), (u, t) -> {
+                    // 非并行流，不会执行第三个参数
+                    System.out.println("u----:" + u);
+                    // 这里的返回值并没有影响返回结果
+                    return null;
+                });
+        System.out.println("add with no parallel:" + reduce1);
+
+        // 并行流
+        Integer reduce2 = strings.parallelStream().flatMap(e -> e.stream()).reduce(0,
+                (acc, e) -> acc + Integer.valueOf(e), (u, t) -> {
+                    // u，t分别为并行流每个子任务的结果
+                    System.out.println("u----:" + u);
+                    System.out.println("t----:" + t);
+                    return u + t;
+                });
+        System.out.println("add with parallel:" + reduce2);
+    }
 
     public static void main(String[] args) {
         System.out.println(formatter.get().format(new Date()));
@@ -29,6 +55,9 @@ public abstract class FunctionTest {
 
         //filter test
         filter(Stream.of("abc","bbb","ddddd"),x->x.contains("b")).forEach(System.out::println);
+
+        //reduce
+        testMultiReduce();
     }
 
     public static int addUp(Stream<Integer> numbers) {
