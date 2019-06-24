808e6fc25fa687732b986a49dbee6159a0364677
dable <daib@glodon.com>
2019-06-24 16:55:02 +0800

    group

---
+++ src/main/java/function/GroupCount.java
@@ -0,0 +1,17 @@
+package function;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GroupCount {
+
+    public static Map<String, Long> count() {
+        return Stream.of("John", "Paul", "George", "John",
+                "Paul", "John").collect(Collectors.groupingBy(k -> k, Collectors.counting()));
+    }
+
+    public static void main(String[] args) {
+        count().forEach((k,v)->System.out.println(k+":"+v));
+    }
+}
