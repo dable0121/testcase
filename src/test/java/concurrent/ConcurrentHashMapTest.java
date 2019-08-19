392f0ac0114602af39ede1053f9b79a11d8c9a25
dable <daib@glodon.com>
2019-08-19 11:23:20 +0800

    concurrenthashmap test

--- src/test/java/concurrent/ConcurrentHashMapTest.java
+++ src/test/java/concurrent/ConcurrentHashMapTest.java
@@ -1,4 +1,17 @@
 package concurrent;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j//不用每次都写logger变量了
 public class ConcurrentHashMapTest {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+        log.info("1---" + map.put("key", "oldValue"));
+        log.info("2---" + map.put("key", "newValue"));
+        log.info("3---" + map.put("key", "newValue2"));
+//        log.info("4---" + map.put("key1", null));
+//        log.info("5---" + map.put(null, ""));
+    }
 }
