c85f0884e863cd4f6e56354e74f90b7bf6272146
dable <daib@glodon.com>
2019-08-22 16:17:49 +0800

    byte int

--- src/test/java/Float.java
+++ src/test/java/Float.java
@@ -4,5 +4,6 @@ public class Float {
     public static void main(String[] args) {
         System.out.println(java.lang.Float.valueOf(999999.00f * 100).intValue());
         System.out.println(new BigDecimal(9999999f).multiply(new BigDecimal(100)).intValue());
+        System.out.println((byte)2 == 2);
     }
 }
