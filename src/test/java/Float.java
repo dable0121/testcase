d5ad1ec07feda25d323ac172698c3b3d9c70823e
dable <daib@glodon.com>
2019-08-28 16:43:09 +0800

    test

--- src/test/java/Float.java
+++ src/test/java/Float.java
@@ -1,9 +1,14 @@
-import java.math.BigDecimal;
+import lombok.extern.slf4j.Slf4j;
 
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
 public class Float {
     public static void main(String[] args) {
-        System.out.println(java.lang.Float.valueOf(999999.00f * 100).intValue());
-        System.out.println(new BigDecimal(9999999f).multiply(new BigDecimal(100)).intValue());
-        System.out.println((byte)2 == 2);
+        log.info("{}", java.lang.Float.valueOf(999999.00f * 100).intValue());
+        log.info("{}", new BigDecimal(9999999f).multiply(new BigDecimal(100)).intValue());
+        log.info("{}", (byte) 2 == 2);
+        log.info("{}", StandardCharsets.UTF_8.name());
     }
 }
