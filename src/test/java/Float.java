5b23a946d74c6444c7656169692bbdfac5370e9d
dable <daib@glodon.com>
2019-08-30 15:10:42 +0800

    value pass

--- src/test/java/Float.java
+++ src/test/java/Float.java
@@ -2,6 +2,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
+import java.text.MessageFormat;
 
 @Slf4j
 public class Float {
@@ -10,5 +11,16 @@ public class Float {
         log.info("{}", new BigDecimal(9999999f).multiply(new BigDecimal(100)).intValue());
         log.info("{}", (byte) 2 == 2);
         log.info("{}", StandardCharsets.UTF_8.name());
+        int i = 10;
+        final int j = i;
+        int k = i;
+        i = 0;
+        System.out.println(MessageFormat.format("i={0}j={1}k = {2}", i, j, k));
+        String ii = "111";
+        final String jj = ii;
+        String kk = ii;
+        ii = "222";
+        kk = ii;
+        System.out.println(MessageFormat.format("ii={0}jj={1}kk={2}", ii, jj, kk));
     }
 }
