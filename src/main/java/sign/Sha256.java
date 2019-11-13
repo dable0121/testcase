0359d83060a2fe0f4abcf5cd86495ae9eeb60b8f
dable <daib@glodon.com>
2019-11-13 09:54:22 +0800

    sign sha256

--- src/main/java/sign/Sha256.java
+++ src/main/java/sign/Sha256.java
@@ -14,7 +14,8 @@ public class Sha256 {
     @SneakyThrows
     public static void main(String[] args) {
         //3ab03839472a23e4efce443a65c398951c3cdab382efe7259cff0b0b4e3069f2
-        String s = DigestUtils.sha256Hex("fdfdfdsfdsfsdfsdf&oR8117iSObyojvJKUPM3kKkztYp8fn5e".getBytes(StandardCharsets.UTF_8.name()));
+        String s =
+                DigestUtils.sha256Hex("2940564527808877488&HSWvbpFwRsS4L53orTyAtqZ87KL5b5s7".getBytes(StandardCharsets.UTF_8.name()));
         log.info("sign={}", s);
     }
 }
