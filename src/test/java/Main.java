4eb33e0569b2086cc8dbbbb93e9ed0f2903d9a68
dable <daib@glodon.com>
2020-04-21 16:41:33 +0800

    LocalDateTime

---
+++ src/test/java/Main.java
@@ -0,0 +1,30 @@
+import com.alibaba.fastjson.JSONObject;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author : dable
+ * @version V1.0
+ * @Description: 测试用
+ * @date Date : 20200421 16:35
+ */
+@Slf4j
+public class Main {
+
+  @Test
+  public void test() {
+    JSONObject json = new JSONObject() {{
+      put("invoiceBornTime", 1587458215000L);
+    }};
+    LocalDateTime invoiceBornTime = json.getDate("invoiceBornTime")
+        .toInstant()
+        .atZone(ZoneId.systemDefault()).toLocalDateTime();
+    log.info(invoiceBornTime.toString());
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
