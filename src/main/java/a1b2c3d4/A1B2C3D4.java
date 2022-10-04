f77721008bcfa3ee3649cf8e2037b3b242bbf41a
dable <dable0121@qq.com>
2022-10-04 22:48:20 +0800

    LockSupport

--- src/main/java/a1b2c3d4/A1B2C3D4.java
+++ src/main/java/a1b2c3d4/A1B2C3D4.java
@@ -2,25 +2,29 @@ package a1b2c3d4;
 
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class A1B2C3D4 {
+    static Thread t1 = null, t2 = null;
     static final char[] c1 = "ABCDEFG".toCharArray();
     static final char[] c2 = "1234567".toCharArray();
 
     public static void main(String[] args) throws InterruptedException {
-        ReentrantLock();
+        ReentrantLockCondition();
 
         Synchronized();
+
+        LockSupportRun();
     }
 
-    public static void ReentrantLock() throws InterruptedException {
-
+    public static void ReentrantLockCondition() throws InterruptedException {
+        System.out.print("ReentrantLockCondition:");
         Lock l = new ReentrantLock();
         Condition cd1 = l.newCondition();
         Condition cd2 = l.newCondition();
 
-        Thread t1 = new Thread(() -> {
+        t1 = new Thread(() -> {
             try {
                 l.lock();
                 for (char c : c1) {
@@ -38,7 +42,7 @@ public class A1B2C3D4 {
         }, "t1");
         t1.start();
 
-        Thread t2 = new Thread(() -> {
+        t2 = new Thread(() -> {
             try {
                 l.lock();
                 for (char c : c2) {
@@ -61,8 +65,9 @@ public class A1B2C3D4 {
 
 
     public static void Synchronized() throws InterruptedException {
+        System.out.print("Synchronized:");
         Object lock = new Object();
-        Thread t1 = new Thread(() -> {
+        t1 = new Thread(() -> {
             synchronized (lock) {
                 for (char c : c1) {
                     System.out.print(c);
@@ -77,7 +82,7 @@ public class A1B2C3D4 {
             }
         }, "t1");
         t1.start();
-        Thread t2 = new Thread(() -> {
+        t2 = new Thread(() -> {
             synchronized (lock) {
                 for (char c : c2) {
                     System.out.print(c);
@@ -95,6 +100,35 @@ public class A1B2C3D4 {
         t1.join();
         t2.join();
         System.out.println("");
+    }
+
+
+    public static void LockSupportRun() {
+        System.out.print("LockSupport:");
+        try {
+            t1 = new Thread(() -> {
+                for (char c : c1) {
+                    LockSupport.unpark(t2);
+                    System.out.print(c);
+                    LockSupport.park();
+                }
+            }, "t1");
+
+            t2 = new Thread(() -> {
+                for (char c : c2) {
+                    LockSupport.park();
+                    System.out.print(c);
+                    LockSupport.unpark(t1);
+                }
+            }, "t2");
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
+            System.out.println("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
